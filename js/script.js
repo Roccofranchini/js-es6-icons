117ea55ba459c2ec71731049e4b614f16a08c634
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-26 15:39:21 +0200

    structure

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+Milestone 1
+Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+Milestone 2
+Coloriamo le icone per 
+
+Milestone 3
+Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+*/
+
+let iconsTemplate = "";
