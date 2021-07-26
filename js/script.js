508356b8a8673ed85ac29c41c7bf2164709373b6
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-26 16:14:44 +0200

    print

--- js/script.js
+++ js/script.js
@@ -9,4 +9,38 @@ Milestone 3
 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 */
 
+// FUNCTIONZ
+
+//cicliamo nell'array e costriamo il nostro icon-template
+
+const printIcons = (icons, targetElement) => {
+	icons.forEach((icon, index) => {
+		let hasOffset = "";
+
+		if (index == 0 || index % 5 == 0) {
+			hasOffset = "offset-md-1";
+		}
+
+		iconsTemplate += `
+        <div class="col-sm-4 col-md-2 ${hasOffset}">
+            <div class="card">
+                <div class="card-body">
+                    <i class="${icon.family} ${icon.prefix}${icon.name}"></i>
+                    <p>${icon.name.toUpperCase()}</p>
+                </div>
+            </div>
+        </div>
+        `;
+	});
+
+	targetElement.innerHTML = iconsTemplate;
+};
+
 let iconsTemplate = "";
+
+console.log(iconsTemplate);
+
+//stampiamo
+
+const cards = document.querySelector("#icons .row");
+printIcons(icons, cards);
